38b73897a3084ddae858b7391d42873b6fc4f64e
wwgstxdylh <43175612+wwgstxdylh@users.noreply.github.com>
2018-10-08 19:12:04 +0800

    homework4

---
+++ Homework4/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Exercise1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            
+
+
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label.Location = new System.Drawing.Point(176, 84);
+            this.label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label.Name = "label1";
+            this.label.Size = new System.Drawing.Size(109, 20);
+            this.label.TabIndex = 0;
+            this.label.Text = "设置时间：";
+            this.label.Click += new System.EventHandler(this.label_Click);
+            // 
+            // timer1
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+           
+
+
+
+            this.maskedTextBox.Location = new System.Drawing.Point(293, 74);
+            this.maskedTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.maskedTextBox.Mask = "90时90分";
+            this.maskedTextBox.Name = "maskedTextBox1";
+            this.maskedTextBox.Size = new System.Drawing.Size(118, 30);
+            this.maskedTextBox.TabIndex = 1;
+            this.maskedTextBox.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox_MaskInputRejected);
+           
+
+
+            this.button1.Location = new System.Drawing.Point(263, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开启闹铃";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+         
+
+
+            this.button2.Location = new System.Drawing.Point(263, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "关闭闹铃";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+           
+
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 320);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox);
+            this.Controls.Add(this.label);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Homework4/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise1
+{
+    public partial class Form1 : Form
+    {
+        //设置变量
+        int flag = 0;//如果为零关闭闹铃，为1则打开
+        int ihour; //小时
+        int iminute; //分钟
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flag = 1; //开启闹铃flag变为1
+
+            //获取用户输入的时间，并转化为整型
+            try
+            {
+                string time = maskedTextBox.Text;
+                string[] time_array = time.Split('时', '分');
+                ihour = Int32.Parse(time_array[0]);
+                iminute = Int32.Parse(time_array[1]);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("您输入的时间有误,请重新输入 ");//重新输入闹铃时间
+            }
+            finally
+            {
+                //flag = 0;//闹铃关闭
+            }
+            //判断用户输入是否有误
+            if ((ihour > 24 && ihour < 0) || (iminute > 60 && iminute < 0))
+            {
+                MessageBox.Show("您输入的时间有误,请重新输入 ");//重新输入闹铃时间
+                flag = 0;//闹铃关闭
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            flag = 0; //关闭闹铃，flag变为0
+            MessageBox.Show("您已关闭闹铃 ");
+        }
+
+        int now_hour, now_minute;
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (flag == 1)
+            {
+                //获取用户输入的时间，并转化为整型
+                string now_time = DateTime.Now.ToString("hh:mm:ss");
+                string[] now_time_array = now_time.Split(':');
+                now_hour = Int32.Parse(now_time_array[0]);
+                now_minute = Int32.Parse(now_time_array[1]);
+
+                //现在时间与用户输入时间进行对比
+                if (now_hour == ihour && now_minute == iminute)
+                {
+                    MessageBox.Show("时间到了 ");
+                    flag = 0;
+                }
+            }
+        }
+    }
+}
